1a230754ecb9fd614e16f9fd995af61f14bed56d
Евгений Геннадьевич Снегиренко <evgeny.snegirenko@es-site.ru>
2025-02-11 09:04:51 +0700

    add js filter car

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
-document.getElementById("main-action-button").onclick = function () {
+window.onload = () => {
+    const filterItems = document.querySelectorAll(".car-filter li");
+    const carItems = document.querySelectorAll(".car");
+    const carsContent = document.getElementById("cars-content");
+
+    filterItems.forEach(item=>{
+        item.onclick = () => {
+            filterItems.forEach(el => el.classList.remove("active"));
+            item.classList.add("active");
+
+            const filterText = item.textContent.toLowerCase();
     
-};
+            carItems.forEach(car => {
+                if (filterText === "все марки" || car.querySelector("h4").textContent.toLowerCase().includes(filterText)) {
+                    car.style.display = "flex";
+                } else {car.style.display = "none"}
+            });
+            carsContent.scrollIntoView({behavior: "instant"});
+        }
+    })
+}
